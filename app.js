331489ed1f09defeca2480bb7d58f13143e81e2e
TimVosch <timbox@live.nl>
2016-07-15 21:23:14 +0200

    Updated to support body-parser and bluebird

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 var app = express();
 
 // Route index
 var routerIndex = require('./routes/index');
+
+mongoose.Promise = require('bluebird');
 
 // Connect to database
 mongoose.connect("mongodb://localhost/test");
 var db = mongoose.connection;
 
 // Start server on succesfull connection
-db.once('open', function() {
+db.once('open', function () {
   console.log('succesfull connection!');
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
   app.use(routerIndex);
 });
 
