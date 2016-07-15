62593f9ab25a0263ad12e1ba78d78cbab2df242b
TimVosch <timbox@live.nl>
2016-07-15 23:44:31 +0200

    Export the schema.

--- schemas/UserAuthSchema.js
+++ schemas/UserAuthSchema.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-var UserAuthSchema = mongoose.Schema({
+module.exports = mongoose.Schema({
   username: {type: String, required: true},
   password: {type: String, required: true},
 });
