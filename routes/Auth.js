1022adff3a61da5578f87bb13a2c1f828321f240
TimVosch <timbox@live.nl>
2016-07-15 23:19:21 +0200

    Created authorization endpoint

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,4 +1,31 @@
 var express = require('express');
-var router = express.Router;
+var router = express.Router();
+var jwt = require('jsonwebtoken');
+
+var UserAuthModel = require('../models/UserAuthModel');
+
+/**
+ * POST Authenticate a user.
+ * This provides a JWT token to use if authenticated correctly.
+ */
+router.post('/', (req, res) => {
+  if (!req.body.username || !req.body.password) {
+    res.status(400).send({ success: false, message: "Username or password missing" });
+    return;
+  }
+  UserAuthModel.findOne({ username: req.body.username }).then((userAuth) => {
+    // Check if the user exists
+    if (!userAuth || req.body.password !== req.body.password) {
+      res.status(403).send({ success: false, message: "Username or password invalid" });
+      return;
+    }
+    // Check if the password is correct
+    var token = jwt.sign({ username: userAuth.username}, req.app.get('JWTSecret'), {
+      expiresIn: 60*60*24 // expires in 1 day
+    });
+
+    res.send({ success: true, token});
+  });
+});
 
 module.exports = router;
