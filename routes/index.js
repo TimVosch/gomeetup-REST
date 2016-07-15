287e9974a147257d3c61c8ddd2862c21171e2fc1
TimVosch <timbox@live.nl>
2016-07-16 00:17:33 +0200

    (debug) endpoint to show permissions for JWTToken

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ var router = express.Router();
 // Routes
 var Authentication = require('./Auth');
 var Events = require('./Events');
+var Permissions = require('./Permissions');
 
 // Load all other routers
 router.use('/auth', Authentication);
-router.use(require('../verification/JwtVerification'));
+router.use(require('../verification/JwtVerification')); // Everything after this requires authentication
 router.use('/events', Events);
+router.use('/permissions', Permissions);
 
 module.exports = router;
