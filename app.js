577c27ec1d56ada0c41ea5cb925408ba7c3605c6
TimVosch <timbox@live.nl>
2016-07-15 22:48:37 +0200

    Improved comments

--- app.js
+++ app.js
@@ -3,26 +3,42 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var app = express();
 
-// Route index
+/**
+ * The base router.
+ * Other routes extend from there
+ */
 var routerIndex = require('./routes/index');
 
+/**
+ * Use bluebird for mongoose promises
+ */
 mongoose.Promise = require('bluebird');
 
-// Connect to database
+/**
+ * Connect to MongoDB server
+ */
 mongoose.connect("mongodb://localhost/test");
 var db = mongoose.connection;
 
-// Start server on succesfull connection
+/**
+ * Start the server when MongoDB connection is succesfull
+ */
 db.once('open', function () {
   console.log('succesfull connection!');
+
+  // parse json body as well as url parameters
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({
     extended: true
   }));
-  app.use(routerIndex);
+
+  // Load the base router
+  app.use('/api', routerIndex);
 });
 
-// Report error on failure
+/**
+ * Report errors
+ */
 db.on('error', console.error.bind(console, 'connection error:'));
 
 module.exports = app;
