24793e8da3e5e800b4dbf60cd30f2bd9397a07cf
TimVosch <timbox@live.nl>
2016-07-16 23:56:20 +0200

    database naming update (kebab)

--- models/user_authentication_model.js
+++ models/user_authentication_model.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose');
 var user_authentication_schema = require('../schemas/user_authentication_schema');
 
-module.exports = mongoose.model('UserAuth', user_authentication_schema);
+module.exports = mongoose.model('user_authentication', user_authentication_schema);
--- models/user_permissions_model.js
+++ models/user_permissions_model.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose');
 var user_permissions_schema = require('../schemas/user_permissions_schema');
 
-module.exports = mongoose.model('UserPermission', user_permissions_schema);
+module.exports = mongoose.model('user_permissions', user_permissions_schema);
