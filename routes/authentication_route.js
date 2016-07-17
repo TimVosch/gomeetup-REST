4e2ade9f12fdc27fe9010215eb3ebdb504aed8f3
TimVosch <timbox@live.nl>
2016-07-17 19:24:14 +0200

    Removed unused requires

--- routes/authentication_route.js
+++ routes/authentication_route.js
@@ -2,9 +2,6 @@ var express = require('express');
 var router = express.Router();
 var api = require('../api/api');
 
-var user_authentication_model = require('../models/user_authentication_model');
-var user_information_model = require('../models/user_information_model');
-
 /**
  * POST Authenticate a user.
  * This provides a JWT token to use if authenticated correctly.
--- routes/events_route.js
+++ routes/events_route.js
@@ -1,9 +1,6 @@
 var express = require('express');
-var mongoose = require('mongoose');
 var router = express.Router();
 var api = require('../api/api');
-
-var event_model = require('../models/event_model.js');
 
 router.get('/', api.events.get_event);
 router.get('/:id', api.events.get_event);
