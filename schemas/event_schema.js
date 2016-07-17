985683546a1d049bb901497f6e975ef3376d8a8b
TimVosch <timbox@live.nl>
2016-07-17 19:10:05 +0200

    new Schema for consistency

--- schemas/event_schema.js
+++ schemas/event_schema.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 // Export the schema
-module.exports = mongoose.Schema({
+module.exports = new mongoose.Schema({
   type: {type: String, required: true},
   name: {type: String, required: true},
   creator: {type: String, required: true},
