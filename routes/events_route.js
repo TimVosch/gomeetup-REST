be4a6a32ceda9b23a6f93207334046083eb07afc
TimVosch <timbox@live.nl>
2016-07-17 21:56:16 +0200

    Implemented permission checks in events route. Should be seen as an example for now. events route will get overhauled soon.

--- routes/events_route.js
+++ routes/events_route.js
@@ -2,9 +2,11 @@ var express = require('express');
 var router = express.Router();
 var api = require('../api/api');
 
-router.get('/', api.events.get_event);
-router.get('/:id', api.events.get_event);
-router.post('/', api.events.create_event);
-router.delete('/:id', api.events.remove_event);
+var check_permissions = require('../middleware/check_permissions')('events');
+
+router.get('/', check_permissions('read'), api.events.get_event);
+router.get('/:id', check_permissions('read'), api.events.get_event);
+router.post('/', check_permissions('create'), api.events.create_event);
+router.delete('/:id', check_permissions('remove'), api.events.remove_event);
 
 module.exports = router;
