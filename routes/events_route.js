14f838388c6df5ec4ff2d1c8f4d5d44fe5c23386
TimVosch <timbox@live.nl>
2016-07-17 19:23:08 +0200

    comments here were redundant

--- routes/events_route.js
+++ routes/events_route.js
@@ -5,22 +5,9 @@ var api = require('../api/api');
 
 var event_model = require('../models/event_model.js');
 
-/**
- * GET returns all or specified event
- */
 router.get('/', api.events.get_event);
 router.get('/:id', api.events.get_event);
-
-
-/**
- * POST creates a new event
- */
 router.post('/', api.events.create_event);
-
-
-/**
- * DELETE removes an event by ID
- */
 router.delete('/:id', api.events.remove_event);
 
 module.exports = router;
