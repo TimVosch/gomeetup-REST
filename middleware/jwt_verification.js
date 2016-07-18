db4c22c3c52117f4657a90a4f3243905f27bed38
TimVosch <timbox@live.nl>
2016-07-18 21:19:54 +0200

    changed succes option to error

--- middleware/jwt_verification.js
+++ middleware/jwt_verification.js
@@ -8,13 +8,13 @@ module.exports = function (req, res, next) {
   if (token) {
     jwt.verify(token, req.app.get('JWTSecret'), (err, decoded) => {
       if (err)
-        res.status(403).send({ success: false, message: "Failed to authenticate token" });
+        res.status(403).send({ error: true, message: "Failed to authenticate token" });
       else {
         // Check blacklist
         revoked_jwt_model.findOne({ jwt_uuid: decoded.uuid }).then((revokedToken) => {
           // If the token is not empty then we're blacklisted
           if (!!revokedToken) {
-            res.status(403).send({ success: false, message: "Token revoked", reason: revokedToken.reason });
+            res.status(403).send({ error: true, message: "Token revoked", reason: revokedToken.reason });
             return;
           }
           req.token = decoded;
@@ -23,6 +23,6 @@ module.exports = function (req, res, next) {
       }
     });
   } else {
-    res.status(403).send({ success: false, message: "No token provided" });
+    res.status(403).send({ error: true, message: "No token provided" });
   }
 };
