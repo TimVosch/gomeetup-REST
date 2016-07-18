a4bd6d6aabd78441306279f67c458a70162c9273
TimVosch <timbox@live.nl>
2016-07-18 21:11:16 +0200

    Included reason for jwt_revoke schema/model

--- schemas/revoked_jwt_schema.js
+++ schemas/revoked_jwt_schema.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 module.exports = new mongoose.Schema({
-  jwt_uuid: {type: String, required: true},
-  expires_at: {type: Number, required: true}
+  jwt_uuid: { type: String, required: true },
+  reason: { type: String, required: false },
+  expires_at: { type: Number, required: true }
 });
