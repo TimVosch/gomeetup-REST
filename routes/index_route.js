fdeee42415735754a4e7a793a76332f1b8b3a491
TimVosch <timbox@live.nl>
2016-07-28 23:57:52 +0200

    Fixed a bug causing 404 to throw 'no jwt' error

--- routes/index_route.js
+++ routes/index_route.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var router = express.Router();
-
+var require_jwt = require('../middleware/jwt_verification');
 // Routes
 var authentication_route = require('./authentication_route');
 var events_route = require('./events_route');
@@ -9,8 +9,7 @@ var permissions_route = require('./permissions_route');
 
 // Load all other routers
 router.use('/authentication', authentication_route);
-router.use(require('../middleware/jwt_verification')); // Everything after this requires authentication
-router.use('/events', events_route);
-router.use('/permissions', permissions_route);
+router.use('/events', require_jwt, events_route);
+router.use('/permissions', require_jwt, permissions_route);
 
 module.exports = router;
