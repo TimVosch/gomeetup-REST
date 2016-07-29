2c50da03ef2c9817151ceebf6b70e97e19a367f1
TimVosch <timbox@live.nl>
2016-07-29 23:27:15 +0200

    jwt_uuid in revoked_jwt_schema should be unique

--- schemas/revoked_jwt_schema.js
+++ schemas/revoked_jwt_schema.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 module.exports = new mongoose.Schema({
-  jwt_uuid: { type: String, required: true },
+  jwt_uuid: { type: String, required: true, unique: true },
   reason: { type: String, required: false },
   expires_at: { type: Date, required: true, expires: 1 }
 });
