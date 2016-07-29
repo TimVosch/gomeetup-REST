f50041ea575309c9633162b34ea28e0ee03e6d7a
TimVosch <timbox@live.nl>
2016-07-29 12:55:06 +0200

    Implemented mongoose' 'unique' modifer for schemas

--- schemas/user_authentication_schema.js
+++ schemas/user_authentication_schema.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 module.exports = new mongoose.Schema({
-  username: {type: String, required: true},
+  username: {type: String, required: true, unique: true},
   password: {type: String, required: true},
   user_id: {type: mongoose.Schema.Types.ObjectId, required: true}
 });
