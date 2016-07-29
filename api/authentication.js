1faedd5b4679869abc1755210b82dacdc103da5a
TimVosch <timbox@live.nl>
2016-07-29 15:40:23 +0200

    updated user_authentication to reference user_info

--- api/authentication.js
+++ api/authentication.js
@@ -25,34 +25,34 @@ module.exports = {};
  */
 module.exports.authenticate_user = (req, res) => {
   if (!req.body.username || !req.body.password) {
-    res.status(400).send({ error: true, message: "User or password missing"});
+    res.status(400).send({ error: true, message: 'User or password missing'});
     return;
   }
-  user_authentication_model.findOne({ username: req.body.username }).then((user_auth) => {
+  user_authentication_model.findOne({ username: req.body.username }).populate('user').then((user_auth) => {
+    console.log(user_auth);
     // Check if the user exists and password is correct (this if check is a bit double/redundant)
     if (!user_auth || req.body.password !== user_auth.password) {
-      res.status(403).send({ error: true, message: "Username or password invalid" });
+      res.status(403).send({ error: true, message: 'Username or password invalid' });
       return;
     }
-
-    // Find the correct permissions for this user
-    user_information_model.findOne({ _id: user_auth.user_id}).then((user_permissions) => {
-      if(!user_permissions) {
-        res.status(500).send({ error: true, message: "No user information associated with " + user_auth.user_id});
-        return;
-      }
-      // Create JWT payload
-      var payload = {
-        uuid: uuid.v4(),
-        user_id: user_auth.id,
-        permissions: user_permissions.permissions
-      };
-      // Sign the token with the payload
-      var token = jwt.sign(payload, req.app.get('JWTSecret'), {
-        expiresIn: req.app.get('jwt_expiration')
-      });
-      res.send({token});
-    })
+    if(!user_auth.user) {
+      res.status(500).send({ error: true, message: 'No user information associated with ' + user_auth.user});
+      return;
+    }
+    // Create JWT payload
+    var payload = {
+      uuid: uuid.v4(),
+      user_id: user_auth.id,
+      permissions: user_auth.user.permissions
+    };
+    // Sign the token with the payload
+    var token = jwt.sign(payload, req.app.get('jwt_secret'), {
+      expiresIn: req.app.get('jwt_expiration')
+    });
+    res.send({token});
+  })
+  .catch(error => {
+    res.status(500).send({ error: true, message: 'Internal server error'});
   });
 };
 
--- schemas/user_authentication_schema.js
+++ schemas/user_authentication_schema.js
@@ -3,5 +3,5 @@ var mongoose = require('mongoose');
 module.exports = new mongoose.Schema({
   username: {type: String, required: true, unique: true, match: /^[\w]{6,}$/},
   password: {type: String, required: true, match: /^.{6,}$/},
-  user_id: {type: mongoose.Schema.Types.ObjectId, required: true}
+  user: {type: mongoose.Schema.Types.ObjectId, required: true, ref: 'user_information'}
 });
