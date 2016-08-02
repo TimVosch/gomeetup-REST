ac31116d9e392f72163ad2500eb770943757f468
TimVosch <timbox@live.nl>
2016-08-02 19:09:30 +0200

    Rewriting unit tests. Cleared previous files. Wrote a helper class. Only user authentication is being tested so far.

--- test/authentication.js
+++ test/authentication.js
@@ -1,171 +1,87 @@
-var assert = require('assert');
+var helper = require('./helper');
 var bluebird = require('bluebird');
-var chai = require('chai');
-var jwt = require('jsonwebtoken');
-var mongoose = require('mongoose');
-var supertest = require('supertest');
+var server = helper.server;
 
-/**
- * BLUEBIRD SETUP
- */
-Promise = bluebird.Promise;
-mongoose.Promise = Promise;
-
-/**
- * CHAI SETUP
- */
-var expect = chai.expect;
-chai.should();
-
-/**
- * SUPERTEST SETUP
- */
-var server = supertest.agent(require('../app'));
-
-/**
- * DATABASE MODEL DEFINITIONS
- */
-var user_information_model = mongoose.model('user_information');
-var user_authentication_model = mongoose.model('user_authentication');
-var test_user_information_model = new user_information_model({
-  _id: mongoose.Types.ObjectId(),
-  first_name: 'John',
-  last_name: 'Snow',
-  email: 'example@example.com',
-  permissions: {
-    'events': ['*'],
-    'jwt': ['*']
-  }
-});
-var test_user_authentication_model = new user_authentication_model({
-  username: 'johnsnow',
-  password: 'testpassword',
-  user: test_user_information_model._id
-});
-
-function populate_database(done) {
-    Promise.all([
-      user_information_model.remove({}),
-      user_authentication_model.remove({})
-    ])
-    .then(function () {
-      return Promise.all([
-        test_user_information_model.save(),
-        test_user_authentication_model.save()
-      ]);
-    })
-    .then(function () {
-      done();
+describe('Authentication endpoints', function() {
+  describe('Authenticating a user', function () {
+    beforeEach(helper.populate_database);
+    describe('Successful', function () {
+      it('should send 200 OK', function(done) {
+        server
+          .get('/api/authentication/user')
+          .auth(helper.dummy.username, helper.dummy.password)
+          .send()
+          .end(function(err, res) {
+            res.status.should.be.equal(200);
+            done();
+          });
+      });
+      it('should return a token', function(done) {
+        server
+          .get('/api/authentication/user')
+          .auth(helper.dummy.username, helper.dummy.password)
+          .send()
+          .end(function(err, res) {
+            expect(res.body.token).to.exist;
+            res.body.token.should.be.a('string');
+            done();
+          });
+      });
     });
-};
-
-function clean_database(done) {
-    Promise.all([
-      user_information_model.remove({}),
-      user_authentication_model.remove({})
-    ])
-    .then(function () {
-      done();
+    describe('Reversed credentials (should not authorize)', function () {
+      it('should send 401 UNAUTHORIZED', function(done) {
+        server
+          .get('/api/authentication/user')
+          .auth(helper.dummy.password, helper.dummy.username)
+          .send()
+          .end(function(err, res) {
+            res.status.should.be.equal(401);
+            done();
+          });
+      });
+      it('should return an error message', function(done) {
+        server
+          .get('/api/authentication/user')
+          .auth(helper.dummy.password, helper.dummy.username)
+          .send()
+          .end(function(err, res) {
+            expect(res.body.error).to.exist;
+            res.body.error.should.be.a('string');
+            done();
+          });
+      });
     });
-}
-
-/**
- * UNIT TEST
- */
-describe('Authentication', function () {
-  /**
-   * TEST SUCCESFUL AUTHENTICATION
-   */
-  describe('Succesful user authentication', function () {
-    before('succesful authentication', populate_database);
-    after('succesful authentication', clean_database);
-    it('should send 200 OK', function(done){
-      server
-        .post('/api/authentication/user')
-        .type('form')
-        .send({
-          username: test_user_authentication_model.username,
-          password: test_user_authentication_model.password
-        })
-        .end(function (err, res) {
-          res.status.should.be.equal(200);
-          done();
-        });
-    });
-    it('should return a token as string', function (done) {
-      server
-        .post('/api/authentication/user')
-        .type('form')
-        .send({
-          username: test_user_authentication_model.username,
-          password: test_user_authentication_model.password
-        })
-        .end(function (err, res) {
-          res.body.should.be.a('object');
-          res.body.token.should.be.a('string');
-          done();
-        });
+    describe('Wrong credentials (should not authorize)', function () {
+      it('should send 401 UNAUTHORIZED', function(done) {
+        server
+          .get('/api/authentication/user')
+          .auth('wrong username', 'wrong password')
+          .send()
+          .end(function(err, res) {
+            res.status.should.be.equal(401);
+            done();
+          });
+      });
+      it('should return an error message', function(done) {
+        server
+          .get('/api/authentication/user')
+          .auth('wrong username', 'wrong password')
+          .send()
+          .end(function(err, res) {
+            expect(res.body.error).to.exist;
+            res.body.error.should.be.a('string');
+            done();
+          });
+      });
     });
   });
-
-  /**
-   * TEST UNSUCCESFUL AUTHENTICATION
-   */
-  describe('Unsuccesful user authentication', function() {
-    before('unsuccesful authentication', populate_database);
-    after('unsuccesful authentication', clean_database);
-    it('should send 403 FORBIDDEN', function(done){
-      server
-        .post('/api/authentication/user')
-        .type('form')
-        .send({
-          username: test_user_authentication_model.username,
-          password: 'wrong password'
-        })
-        .end(function (err, res) {
-          res.status.should.be.equal(403);
-          done();
-        });
+  describe('Creating a user', function () {
+    describe('Successful', function () {
+      
     });
-    it('should contain error message in body', function(done) {
-      server
-        .post('/api/authentication/user')
-        .type('form')
-        .send({
-          username: test_user_authentication_model.username,
-          password: 'wrong password'
-        })
-        .end(function (err, res) {
-          res.body.error.should.be.a('string');
-          done();
-        });
-    });
-  });
-
-/**
- * TEST USER CREATION
- */
-  describe('Succesful account creation', function() {
-    before('account creation', clean_database);
-    after('account creation', clean_database);
-    it('should return 200 OK', function(done) {
-      server
-        .put('/api/authentication/user')
-        .type('form')
-        .send({
-          username: test_user_authentication_model.username,
-          password: test_user_authentication_model.password,
-          first_name: user.test_user_information_model.first_name,
-          last_name: test_user_information_model.last_name,
-          email: test_user_information_model.email
-        })
-        .end(function (err, res) {
-          res.status.should.be.equal(200);
-          done();
-        });
+    describe('Unsuccessful', function () {
+      beforeEach(helper.populate_database);
+      
     });
   });
 });
-/**
- * END OF UNIT TEST
- */
---
+++ test/helper.js
@@ -0,0 +1,79 @@
+/**
+ * SET ENV VARIABLES
+ */
+//
+process.env.NODE_ENV = 'testing';
+process.env.DEBUG = '';
+
+var mongoose = require('mongoose');
+var supertest = require('supertest');
+var bluebird = require('bluebird');
+var app = require('../app');
+var server = supertest.agent(app);
+var clearDB = require('mocha-mongoose')('mongodb://localhost/gomeetup_test');
+var chai = require('chai');
+Promise = bluebird.Promise;
+chai.should();
+expect = chai.expect;
+
+
+/**
+ * Pre-logic defined here
+ */
+
+// Clear database
+clearDB(function(err) {
+  if (err)
+    throw err;
+});
+
+/**
+ * Export functions and variables defined after this line
+ */
+var helper = {};
+
+/**
+ * Dummy data
+ */
+helper.dummy = {
+  username: 'johnsnow',
+  password: 'testpassword',
+  first_name: "John",
+  last_name: "Snow",
+  email: "example@example.com",
+  permissions: {
+    event: ['read']
+  }
+};
+
+/**
+ * Populates the database with some dummy data
+ */
+helper.populate_database = (done) => {
+  var user_information = new (mongoose.model('user_information'))({
+    _id: mongoose.Types.ObjectId(),
+    first_name: helper.dummy.first_name,
+    last_name: helper.dummy.last_name,
+    email: helper.dummy.email,
+    permissions: helper.dummy.permissions
+  });
+  var user_authentication = new (mongoose.model('user_authentication'))({
+    username: helper.dummy.username,
+    password: helper.dummy.password,
+    user: user_information._id
+  });
+  Promise.all([
+    user_information.save(),
+    user_authentication.save()
+  ]).then(function() {
+    if (!!done)
+      done();
+  });
+}
+
+/**
+ * Supertest client
+ */
+helper.server = server;
+
+module.exports = helper;
