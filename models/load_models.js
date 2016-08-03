53638950c837229007eafb258f724277f1a8a67a
TimVosch <timbox@live.nl>
2016-08-03 14:22:28 +0200

    Removed previous load_model remnants :P

--- models/load_models.js
+++ models/load_models.js
@@ -1,19 +1,3 @@
-// module.exports.models = [
-//   "event_model",
-//   "revoked_jwt_model",
-//   "user_authentication_model",
-//   "user_information_model"
-// ];
-
-// module.exports.load = () => {
-//   for (var key in module.exports.models) {
-//     if (module.exports.models.hasOwnProperty(key)) {
-//       var element = module.exports.models[key];
-//       require('./' + element)(); // Load the model file
-//     }
-//   }
-// };
-
 var mongoose = require('mongoose');
 
 module.exports.models = [
