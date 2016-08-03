8afb2c9be4f9e063843c22a2950967191cf2fb63
TimVosch <timbox@live.nl>
2016-08-03 21:34:02 +0200

    Change nearby events endpoint. now /events/nearby/:lng/:lat/:dist

--- routes/events_route.js
+++ routes/events_route.js
@@ -4,7 +4,7 @@ var api = require('../api/api');
 
 var check_permissions = require('../middleware/check_permissions')('events');
 
-router.get('/:longitude/:latitude/:distance?', check_permissions('nearby'), api.events.get_nearby_events)
+router.get('/nearby/:longitude/:latitude/:distance?', check_permissions('nearby'), api.events.get_nearby_events)
 router.get('/:ids', check_permissions('read'), api.events.get_event);
 // router.post('/', check_permissions('create'), api.events.create_event);
 // router.delete('/:id', check_permissions('remove'), api.events.remove_event);
